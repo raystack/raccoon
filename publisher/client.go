cf88de919f28da2406e2836a7f70c649a801cb29
Chakravarthy Varaga P <chakravarthyvp@gmail.com>
2020-06-05 17:48:47 +0530

    [Chakravarthy] - Adding topic, changes with kafka code for graceful shutdown

--- publisher/client.go
+++ publisher/client.go
@@ -5,12 +5,12 @@ import (
 	"clickstream-service/logger"
 	"context"
 	"fmt"
-	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"gopkg.in/errgo.v2/errors"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"gopkg.in/errgo.v2/errors"
 )
 
 // check producer is already created or not, if already
@@ -20,7 +20,7 @@ type Producer struct {
 	topic    string
 }
 
-func NewProducer(ctx context.Context,kc config.KafkaConfig) (*Producer, error) {
+func NewProducer(ctx context.Context, kc config.KafkaConfig) (*Producer, error) {
 
 	kafkaConfigMap := &kafka.ConfigMap{
 		"bootstrap.servers": kc.BrokerList(),
@@ -29,19 +29,20 @@ func NewProducer(ctx context.Context,kc config.KafkaConfig) (*Producer, error) {
 	producer, err := kafka.NewProducer(kafkaConfigMap)
 
 	if err != nil {
-		logger.Error(fmt.Sprintf("Error while creating new kafka producer. %s",err))
-		return nil, errors.New(fmt.Sprintf("Error while creating new kafka producer. %s",err))
+		logger.Error(fmt.Sprintf("Error while creating new kafka producer. %s", err))
+		return nil, errors.New(fmt.Sprintf("Error while creating new kafka producer. %s", err))
 	}
 	logger.Info("kafka producer created", producer)
 
-	newProducer :=  &Producer{
+	newProducer := &Producer{
 		producer: producer,
+		topic: kc.Topic(),
 	}
-    go closeProducer(ctx, newProducer)
+	go shutProducerGracefully(ctx, newProducer)
 	return newProducer, nil
 }
 
-func closeProducer(ctx context.Context, p *Producer) {
+func shutProducerGracefully(ctx context.Context, p *Producer) {
 	signalChan := make(chan os.Signal)
 	signal.Notify(signalChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	for {
@@ -49,10 +50,8 @@ func closeProducer(ctx context.Context, p *Producer) {
 		switch sig {
 		case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
 			logger.Info(fmt.Sprintf("[Kafka.Producer] Received a signal %s", sig))
-			logger.Info(fmt.Sprintf("[Kafka.Producer] waiting for 3 secs grace period before shutdown ", ))
-			time.Sleep(3 * time.Second)
-			logger.Info("Closing Producer")
 			p.producer.Close()
+			logger.Info("Closed Producer")
 		default:
 			logger.Info(fmt.Sprintf("[Kafka.Producer] Received a unexpected signal %s", sig))
 		}
@@ -62,7 +61,7 @@ func closeProducer(ctx context.Context, p *Producer) {
 func (p *Producer) PublishMessage(msg, key []byte) error {
 	err := p.producer.Produce(&kafka.Message{
 		TopicPartition: kafka.TopicPartition{
-			Topic:      &p.topic,
+			Topic:     &p.topic,
 			Partition: kafka.PartitionAny,
 		},
 		Value: msg,

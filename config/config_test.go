fe5ea74c43705d8ec06fb0401a48b98b4146d0ef
Rasyid Hakim <rasyid.xyz@gmail.com>
2020-06-17 10:17:03 +0530

    Expose kafka's retries and backoff config

--- config/config_test.go
+++ config/config_test.go
@@ -26,13 +26,17 @@ func TestNewKafkaConfig(t *testing.T) {
 	os.Setenv("KAFKA_ACKS", "1")
 	os.Setenv("KAFKA_QUEUE_SIZE", "10000")
 	os.Setenv("KAFKA_FLUSH_INTERVAL", "1000")
+	os.Setenv("KAFKA_RETRIES", "2")
+	os.Setenv("KAFKA_RETRY_BACKOFF_MS", "100")
 
 	expectedKafkaConfig := KafkaConfig{
-		brokerList:   "kafka:6668",
-		topic:        "test1",
-		acks:         1,
-		maxQueueSize: 10000,
-		flushInterval: 1000,
+		brokerList:     "kafka:6668",
+		topic:          "test1",
+		acks:           1,
+		maxQueueSize:   10000,
+		flushInterval:  1000,
+		retries:        2,
+		retryBackoffMs: 100,
 	}
 
 	kafkaConfig := NewKafkaConfig()

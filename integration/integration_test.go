c9cfc0477bb2a93728850fd201d89461127a6e7a
Rasyid Hakim <rasyid.xyz@gmail.com>
2020-07-06 15:01:59 +0530

    Add integration test

---
+++ integration/integration_test.go
@@ -0,0 +1,193 @@
+package integration
+
+import (
+	"fmt"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"math/rand"
+	"net/http"
+	"source.golabs.io/mobile/clickstream-go-proto/gojek/clickstream/de"
+	"testing"
+	"time"
+)
+
+var uuid string
+var timeout time.Duration
+var topic string
+var url string
+var bootstrapServers string
+
+func TestMain(m *testing.M) {
+	uuid = fmt.Sprintf("%d-test", rand.Int())
+	timeout = 120 * time.Second
+	topic = "de-test-raccoon"
+	url = "wss://raccoon-integration.gojekapi.com/api/v1/events"
+	bootstrapServers = "10.200.188.27:6668"
+	m.Run()
+}
+
+func TestSuite(t *testing.T) {
+	accessToken, err := FetchAccessToken()
+	assert.NoError(t, err)
+	header := http.Header{
+		"Authorization": []string{fmt.Sprintf("Bearer %v", accessToken)},
+	}
+	t.Run("Should response with BadRequest when sending invalid request", func(t *testing.T) {
+		wss, _, err := websocket.DefaultDialer.Dial(url, header)
+
+		if err != nil {
+			assert.Fail(t, fmt.Sprintf("fail to connect. %v", err))
+		}
+
+		wss.WriteMessage(websocket.BinaryMessage, []byte{1})
+
+		mType, resp, err := wss.ReadMessage()
+		r := &de.EventResponse{}
+		_ = proto.Unmarshal(resp, r)
+		assert.Equal(t, mType, websocket.BinaryMessage)
+		assert.Empty(t, err)
+		assert.Equal(t, de.Status_ERROR, r.Status)
+		assert.Equal(t, de.Code_BAD_REQUEST, r.Code)
+		assert.NotEmpty(t, r.Reason)
+		assert.Empty(t, r.Data)
+
+		wss.WriteControl(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""), time.Now().Add(100*time.Millisecond))
+	})
+
+	t.Run("Should response with success when request is processed", func(t *testing.T) {
+		wss, _, err := websocket.DefaultDialer.Dial(url, header)
+
+		if err != nil {
+			panic(err)
+		}
+
+		req := &de.EventRequest{
+			ReqGuid:  "1234",
+			SentTime: ptypes.TimestampNow(),
+			Data: []*de.CSEventMessage{{
+				EventGuid:      uuid,
+				EventType:      "ride",
+				EventName:      "ride",
+				EventTimestamp: ptypes.TimestampNow()},
+			},
+		}
+		bReq, _ := proto.Marshal(req)
+		wss.WriteMessage(websocket.BinaryMessage, bReq)
+
+		mType, resp, err := wss.ReadMessage()
+		r := &de.EventResponse{}
+		_ = proto.Unmarshal(resp, r)
+		assert.Equal(t, mType, websocket.BinaryMessage)
+		assert.Empty(t, err)
+		assert.Equal(t, r.Code, de.Code_OK)
+		assert.Equal(t, r.Status, de.Status_SUCCESS)
+		assert.Equal(t, r.Reason, "")
+		assert.Equal(t, r.Data, map[string]string{"req_guid": "1234"})
+		fmt.Println(fmt.Sprintf("gen uuid: %v", uuid))
+
+		wss.WriteControl(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""), time.Now().Add(100*time.Millisecond))
+	})
+
+	t.Run("Should be able to consume published message", func(t *testing.T) {
+		c, err := kafka.NewConsumer(&kafka.ConfigMap{
+			"bootstrap.servers": bootstrapServers,
+			"group.id":          "my-local-group",
+			"auto.offset.reset": "earliest",
+		})
+
+		if err != nil {
+			assert.Fail(t, "setup kafka consumer failed")
+		}
+
+		e := c.Subscribe(topic, nil)
+		if e != nil {
+			assert.Fail(t, fmt.Sprintf("Pls try again. %v", e))
+		}
+
+		for {
+			select {
+			case <-time.After(timeout):
+				assert.Fail(t, "timeout")
+				return
+			default:
+				msg, err := c.ReadMessage(-1)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				m := &de.CSEventMessage{}
+				proto.Unmarshal(msg.Value, m)
+				fmt.Println(uuid)
+				if m.EventGuid == uuid {
+					return
+				}
+			}
+		}
+
+	})
+
+	t.Run("Should close connection when client is unresponsive", func(t *testing.T) {
+		wss, _, err := websocket.DefaultDialer.Dial(url, header)
+
+		if err != nil {
+			assert.Fail(t, err.Error())
+		}
+
+		wss.SetPingHandler(func(appData string) error {
+			return nil
+		})
+
+		done := make(chan int)
+		wss.SetCloseHandler(func(code int, text string) error {
+			close(done)
+			return nil
+		})
+
+		go func() { _, _, err = wss.ReadMessage() }()
+		select {
+		case <-time.After(timeout):
+			break
+		case <-done:
+			break
+		}
+
+		assert.Error(t, err)
+	})
+
+	t.Run("Should disconnect subsequence connection from same user when already connected", func(t *testing.T) {
+		done := make(chan int)
+		_, _, err := websocket.DefaultDialer.Dial(url, header)
+
+		assert.NoError(t, err)
+
+		secondWss, _, err := websocket.DefaultDialer.Dial(url, header)
+
+		assert.NoError(t, err)
+
+		secondWss.SetCloseHandler(func(code int, text string) error {
+			assert.Equal(t, code, websocket.ClosePolicyViolation)
+			close(done)
+			return err
+		})
+
+		go func() {
+			for {
+				_, _, err := secondWss.ReadMessage()
+				if err != nil {
+					break
+				}
+			}
+		}()
+		select {
+		case <-time.After(timeout):
+			assert.Fail(t, "Timeout. Expecting second connection to close")
+			break
+		case <-done:
+			break
+		}
+	})
+
+}

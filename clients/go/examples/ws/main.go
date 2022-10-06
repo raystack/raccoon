6fcb7b8855a9e88a012cff7ff9fca538dcaf89b1
Akbar Shaikh <aashaikh55@gmail.com>
2022-10-06 16:27:04 +0530

    feat: websocket client changes

---
+++ clients/go/examples/ws/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	raccoon "github.com/odpf/raccoon/clients/go"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/google/uuid"
+	"github.com/odpf/raccoon/clients/go/testdata"
+	"github.com/odpf/raccoon/clients/go/ws"
+)
+
+func main() {
+	client, err := ws.New(
+		ws.WithUrl("ws://localhost:8080/api/v1/events"),
+		ws.WithHeader("x-user-id", "123"),
+		ws.WithHeader("x-user-type", "gojek"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	reqGuid, err := client.Send([]*raccoon.Event{
+		{
+			Type: "page",
+			Data: &testdata.PageEvent{
+				EventGuid: uuid.NewString(),
+				EventName: "clicked",
+				SentTime:  timestamppb.Now(),
+			},
+		},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(reqGuid)
+	resp := <-client.EventAcks()
+	fmt.Println(resp.Status)
+	fmt.Println(resp.Data["req_guid"])
+}
---
+++ clients/go/ws/option.go
@@ -0,0 +1,67 @@
+package ws
+
+import (
+	"time"
+
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	raccoon "github.com/odpf/raccoon/clients/go"
+	"github.com/odpf/raccoon/clients/go/log"
+	"github.com/odpf/raccoon/clients/go/serializer"
+	"github.com/odpf/raccoon/clients/go/wire"
+)
+
+// Rest is the http implementation
+type WS struct {
+	raccoon.Client
+	url       string
+	serialize serializer.SerializerFunc
+	wire      wire.WireMarshaler
+	conn      *websocket.Conn
+	headers   http.Header
+	retryWait time.Duration
+	retryMax  uint
+	logger    log.Logger
+	acks      chan *raccoon.Response
+}
+
+// Option represents the rest client options.
+type Option func(*WS)
+
+// WithUrl sets the service address
+func WithUrl(url string) Option {
+	return func(rc *WS) {
+		rc.url = url
+	}
+}
+
+// WithSerializer sets the serializer for the raccoon message.
+func WithSerializer(s serializer.SerializerFunc) Option {
+	return func(rc *WS) {
+		rc.serialize = s
+	}
+}
+
+// WithHeader sets the http header for the request.
+func WithHeader(key, val string) Option {
+	return func(rc *WS) {
+		rc.headers.Add(key, val)
+	}
+}
+
+// WithRetry retries for the error upto max attempts with the given delay between calls
+func WithRetry(delay time.Duration, maxAttempts uint) Option {
+	return func(rc *WS) {
+		rc.retryMax = maxAttempts
+		rc.retryWait = delay
+	}
+}
+
+// WithLogger sets the logger for the client.
+func WithLogger(logger log.Logger) Option {
+	return func(r *WS) {
+		log.SetLogger(logger)
+		r.logger = log.Default()
+	}
+}

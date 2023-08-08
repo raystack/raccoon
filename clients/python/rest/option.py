d22ce1d3a4d1b6f813199c538074d6c5553b5302
Punit Kulal <punitkulal1996@gmail.com>
2023-08-08 21:11:53 +0530

    added a config builder

--- clients/python/rest/option.py
+++ clients/python/rest/option.py
@@ -6,3 +6,29 @@ class RestClientConfig:
     max_retries: int
     content_type: ContentType
 
+
+class RestClientConfigBuilder:
+
+    def __init__(self):
+        self.config = RestClientConfig()
+
+    def with_url(self, url):
+        self.config.url = url
+        return self
+
+    def with_retry_count(self, retry_count):
+        if not isinstance(retry_count, int):
+            raise ValueError("retry_count should be an integer")
+        elif retry_count > 10:
+            raise ValueError("retry should not be greater than 10")
+        self.config.max_retries = retry_count
+        return self
+
+    def with_content_type(self, content_type):
+        if not isinstance(content_type, ContentType):
+            raise ValueError("invalid  serialiser/deserialiser type")
+        self.config.content_type = content_type
+        return self
+
+    def build(self):
+        return self.config

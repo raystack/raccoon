e23e1e8c82804c80a9ac0ef4f3c1e118f02715d1
Punit Kulal <punitkulal1996@gmail.com>
2023-08-17 09:36:31 +0530

    added tests wrt wire type changes and refactoring

--- clients/python/serde/json_serde.py
+++ clients/python/serde/json_serde.py
@@ -9,13 +9,13 @@ from serde.wire import Wire
 
 class JsonSerde(Serde, Wire):
     def serialise(self, event):
-        return bytes(json.dumps(event), "utf-8")
+        return bytes(json.dumps(event), "utf-8")  # uses json.dumps since the input can be either protobuf message or dictionary
 
     def get_content_type(self):
         return "application/json"
 
     def marshal(self, event):
-        return json_format.MessageToJson(event)
+        return json_format.MessageToJson(event)  # uses json_format since the event is always a protobuf message
 
     def unmarshal(self, data, template):
         return json_format.Parse(data, template)
--- clients/python/tests/unit/rest/client_test.py
+++ clients/python/tests/unit/rest/client_test.py
@@ -6,37 +6,52 @@ from unittest import mock
 from unittest.mock import patch
 
 import requests
-from google.protobuf import json_format
+from google.protobuf import json_format, timestamp_pb2
 
 import client
 from protos.raystack.raccoon.v1beta1.raccoon_pb2 import SendEventRequest, SendEventResponse, Status
 from rest.client import RestClient
 from rest.option import RestClientConfigBuilder
-from serde.enum import Serialiser
+from serde.enum import Serialiser, WireType
+
+
+def _get_static_uuid():
+    return "17e2ac19-df8b-4a30-b111-fd7f5073d2f5"
+
+
+def _get_static_time():
+    return 1692182259
 
 
 class RestClientTest(unittest.TestCase):
     sample_url = "http://localhost:8080/api/v1/"
     max_retries = 3
-    content_type = Serialiser.JSON
+    serialiser = Serialiser.JSON
+    wire_type = WireType.JSON
 
-    def test_client_creation(self):
+    def test_client_creation_success(self):
         client_config = RestClientConfigBuilder(). \
             with_url(self.sample_url). \
-            with_serialiser(self.content_type). \
-            with_retry_count(self.max_retries).build()
+            with_serialiser(self.serialiser). \
+            with_retry_count(self.max_retries). \
+            with_wire_type(self.wire_type).build()
         rest_client = RestClient(client_config)
         self.assertEqual(rest_client.url, self.sample_url, "sample_urls do not match")
         self.assertEqual(rest_client.session.adapters["https://"].max_retries.total, self.max_retries)
         self.assertEqual(rest_client.session.adapters["http://"].max_retries.total, self.max_retries)
-        self.assertEqual(type(rest_client.serde), self.content_type.value, "content type is configured incorrectly")
+        self.assertEqual(type(rest_client.serde), self.serialiser.value, "serialiser is configured incorrectly")
+        self.assertEqual(type(rest_client.wire), self.wire_type.value, "wire type is configured incorrectly")
 
-    def test_get_stub_request(self):
-        client = self._get_rest_client()
-
-        with patch("rest.client.uuid.uuid4", return_value=self._get_static_uuid()):
-            req = client._get_stub_request()
-            self.assertEqual(req.req_guid, self._get_static_uuid())
+    @patch("rest.client.time.time_ns", return_value=_get_static_time())
+    def test_get_stub_request(self, time_ns):
+        rest_client = self._get_rest_client()
+        ts = timestamp_pb2.Timestamp()
+        ts.FromNanoseconds(time.time_ns())
+        with patch("rest.client.uuid.uuid4", return_value=_get_static_uuid()):
+            req = rest_client._get_stub_request()
+            self.assertEqual(req.req_guid, _get_static_uuid())
+            self.assertEqual(req.sent_time.seconds, ts.seconds)
+            self.assertEqual(req.sent_time.nanos, ts.nanos)
 
     def test_client_send(self):
         session_mock = mock.Mock()
@@ -46,8 +61,8 @@ class RestClientTest(unittest.TestCase):
         event_arr = [self._get_stub_event_payload()]
         req = SendEventRequest()
         expected_req = SendEventRequest()
-        expected_req.req_guid = self._get_static_uuid()
-        req.req_guid = self._get_static_uuid()
+        expected_req.req_guid = _get_static_uuid()
+        req.req_guid = _get_static_uuid()
         time_in_ns = time.time_ns()
         req.sent_time.FromNanoseconds(time_in_ns)
         expected_req.sent_time.FromNanoseconds(time_in_ns)
@@ -68,21 +83,21 @@ class RestClientTest(unittest.TestCase):
         rest_client = self._get_rest_client()
         deserialised_response = rest_client._parse_response(resp)
         self.assertEqual(deserialised_response.status, Status.STATUS_SUCCESS)
-        self.assertEqual(deserialised_response.data["req_guid"], self._get_static_uuid())
-        self.assertEqual(deserialised_response.sent_time, self._get_static_time())
+        self.assertEqual(deserialised_response.data["req_guid"], _get_static_uuid())
+        self.assertEqual(deserialised_response.sent_time, _get_static_time())
 
     def _get_rest_client(self):
         client_config = RestClientConfigBuilder(). \
             with_url(self.sample_url). \
-            with_serialiser(self.content_type). \
+            with_serialiser(self.serialiser). \
             with_retry_count(self.max_retries).build()
         return RestClient(client_config)
 
     def _get_stub_response(self):
         response = requests.Response()
         response.status_code = requests.status_codes.codes["ok"]
-        json_response = {"status": 1, "code": 1, "sent_time": self._get_static_time(),
-                         "data": {"req_guid": self._get_static_uuid()}}
+        json_response = {"status": 1, "code": 1, "sent_time": _get_static_time(),
+                         "data": {"req_guid": _get_static_uuid()}}
         json_string2 = json.dumps(json_response)
         response._content = json_string2
         return response
@@ -92,12 +107,6 @@ class RestClientTest(unittest.TestCase):
         e.type = "random_topic"
         e.event = {"a": "abc"}
         return e
-
-    def _get_static_uuid(self):
-        return "17e2ac19-df8b-4a30-b111-fd7f5073d2f5"
-
-    def _get_static_time(self):
-        return 1692182259
 
     def _get_send_event_response(self):
         serialised_data = SendEventResponse()

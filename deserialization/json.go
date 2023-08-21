7b80220cd5c4a8b2bb1099fea24ef15d6a421d94
Punit Kulal <punitkulal1996@gmail.com>
2023-08-21 18:56:14 +0530

    use protojson deserialiser for deserialising json wire type instead of json.marshal due to buggy support for deserialising json messages

--- deserialization/json.go
+++ deserialization/json.go
@@ -1,7 +1,16 @@
 package deserialization
 
-import "encoding/json"
+import (
+	"fmt"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
 
 func DeserializeJSON(b []byte, i interface{}) error {
-	return json.Unmarshal(b, i)
+	message, ok := i.(proto.Message)
+	if !ok {
+		return fmt.Errorf("expected a valid json which could be decoded into a protobuf")
+	}
+	return protojson.Unmarshal(b, message)
 }

2fa680db290cf58841d72d49f65b727668256247
Punit Kulal <punitkulal1996@gmail.com>
2023-08-31 00:26:47 +0530

    feat: init prometheus collector

---
+++ metrics/prometheus.go
@@ -0,0 +1,65 @@
+package metrics
+
+import "github.com/prometheus/client_golang/prometheus"
+
+type PrometheusCollector struct {
+	counters  map[string]*prometheus.CounterVec
+	gauges    map[string]prometheus.Gauge
+	histogram map[string]prometheus.Histogram
+}
+
+func initPrometheusCollector() *PrometheusCollector {
+	return &PrometheusCollector{
+		counters:  getCounterMap(),
+		gauges:    getGaugeMap(),
+		histogram: getHistogramMap(),
+	}
+}
+
+func getCounterMap() map[string]*prometheus.CounterVec {
+	counters := make(map[string]*prometheus.CounterVec)
+	counters["kafka_messages_delivered_total"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "kafka_messages_delivered_total",
+		Help: "Number of delivered events to Kafka"}, []string{"success", "conn_group", "event_type"})
+	counters["events_rx_bytes_total"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "events_rx_bytes_total",
+		Help: "Total byte receieved in requests"}, []string{"conn_group", "event_type"})
+	counters["events_rx_total"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "events_rx_total",
+		Help: "Number of events received in requests"}, []string{"conn_group", "event_type"})
+	counters["kafka_unknown_topic_failure_total"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "kafka_unknown_topic_failure_total",
+		Help: "Number of delivery failure caused by topic does not exist in kafka."}, []string{"topic", "event_type"})
+	counters["batches_read_total"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "batches_read_total",
+		Help: "Request count"}, []string{"status", "reason", "conn_group"})
+	counters["events_duplicate_total"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "events_duplicate_total",
+		Help: "Total Number of Duplicate events recieved by the server"}, []string{"conn_group", "reason"})
+	counters["server_ping_failure_total"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "server_ping_failure_total",
+		Help: "Total ping that server fails to send"}, []string{"conn_group"})
+	counters["conn_close_err_count"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "conn_close_err_count",
+		Help: "Total Number of Connection Errors When Trying To Close"}, []string{})
+	counters["user_connection_failure_total"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "user_connection_failure_total",
+		Help: "Number of fail connections established to the server"}, []string{"conn_group", "reason"})
+	counters["user_connection_success_total"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "user_connection_success_total",
+		Help: "Number of successful connections established to the server"}, []string{"conn_group"})
+	counters["server_pong_failure_total"] = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "server_pong_failure_total",
+		Help: "Total pong that server fails to send"}, []string{"conn_group"})
+	return counters
+}
+
+func getGaugeMap() map[string]prometheus.Gauge {
+	gauges := make(map[string]prometheus.Gauge)
+	return gauges
+}
+
+func getHistogramMap() map[string]prometheus.Histogram {
+	histogram := make(map[string]prometheus.Histogram)
+	return histogram
+}

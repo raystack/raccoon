da489d14687301d585d8f7ecd686b350193ee430
turtleDev <saravjeetamansingh@gmail.com>
2024-06-20 20:46:22 +0530

    publisher: kinesis: add tests for stream existence

--- publisher/kinesis/kinesis.go
+++ publisher/kinesis/kinesis.go
@@ -1,13 +1,117 @@
 package kinesis
 
-import pb "github.com/raystack/raccoon/proto"
+import (
+	"cmp"
+	"context"
+	"errors"
+	"strings"
+	"time"
 
-type Publisher struct{}
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/kinesis"
+	"github.com/aws/aws-sdk-go-v2/service/kinesis/types"
+	pb "github.com/raystack/raccoon/proto"
+	"github.com/raystack/raccoon/publisher"
+)
+
+type Publisher struct {
+	client            *kinesis.Client
+	streamPattern     string
+	streamAutocreate  bool
+	defaultShardCount int32
+}
 
 func (p *Publisher) ProduceBulk(events []*pb.Event, connGroup string) error {
+	errors := make([]error, len(events))
+	for order, event := range events {
+		streamName := strings.Replace(p.streamPattern, "%s", event.Type, 1)
+		_, err := p.stream(streamName)
+		if err != nil {
+			errors[order] = err
+			continue
+		}
+
+	}
+	if cmp.Or(errors...) != nil {
+		return &publisher.BulkError{Errors: errors}
+	}
 	return nil
 }
 
-func New() *Publisher {
-	return &Publisher{}
+func (p *Publisher) stream(name string) (string, error) {
+	stream, err := p.client.DescribeStreamSummary(
+		context.Background(),
+		&kinesis.DescribeStreamSummaryInput{
+			StreamName: aws.String(name),
+		},
+	)
+
+	if err != nil {
+		var errNotFound *types.ResourceNotFoundException
+		if !errors.As(err, &errNotFound) {
+			return "", err
+		}
+
+		if !p.streamAutocreate {
+			return "", errNotFound
+		}
+
+		_, err := p.client.CreateStream(
+			context.Background(),
+			&kinesis.CreateStreamInput{
+				StreamName: aws.String(name),
+				ShardCount: aws.Int32(p.defaultShardCount),
+				StreamModeDetails: &types.StreamModeDetails{
+					StreamMode: types.StreamModeOnDemand, // TODO: make this configurable
+				},
+			},
+		)
+		if err != nil {
+			return "", err
+		}
+		stream, err = p.client.DescribeStreamSummary(
+			context.Background(),
+			&kinesis.DescribeStreamSummaryInput{
+				StreamName: aws.String(name),
+			},
+		)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	for stream.StreamDescriptionSummary.StreamStatus != types.StreamStatusActive {
+		time.Sleep(1 * time.Second)
+		stream, err = p.client.DescribeStreamSummary(
+			context.Background(),
+			&kinesis.DescribeStreamSummaryInput{
+				StreamName: aws.String(name),
+			},
+		)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return *stream.StreamDescriptionSummary.StreamARN, nil
+}
+
+type Opt func(*Publisher)
+
+func WithStreamAutocreate(autocreate bool) Opt {
+	return func(p *Publisher) {
+		p.streamAutocreate = autocreate
+	}
+}
+
+func New(client *kinesis.Client, opts ...Opt) *Publisher {
+	p := &Publisher{
+		client:            client,
+		streamPattern:     "%s",
+		defaultShardCount: 1,
+	}
+	for _, opt := range opts {
+		opt(p)
+	}
+	return p
 }
--- publisher/kinesis/kinesis_test.go
+++ publisher/kinesis/kinesis_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	kinesis_sdk "github.com/aws/aws-sdk-go-v2/service/kinesis"
+	pb "github.com/raystack/raccoon/proto"
+	"github.com/raystack/raccoon/publisher/kinesis"
 	"github.com/stretchr/testify/require"
 )
 
@@ -32,6 +34,13 @@ func withLocalStack(host string) func(o *kinesis_sdk.Options) {
 	}
 }
 
+var (
+	testEvent = &pb.Event{
+		EventBytes: []byte("EVENT"),
+		Type:       "click",
+	}
+)
+
 func TestKinesisProducer(t *testing.T) {
 
 	localstackHost := os.Getenv(envLocalstackHost)
@@ -42,5 +51,21 @@ func TestKinesisProducer(t *testing.T) {
 	cfg, err := config.LoadDefaultConfig(context.Background())
 	require.NoError(t, err, "error loading aws config")
 
-	_ = kinesis_sdk.NewFromConfig(cfg, withLocalStack(localstackHost))
+	client := kinesis_sdk.NewFromConfig(cfg, withLocalStack(localstackHost))
+
+	t.Run("should return an error if stream doesn't exist", func(t *testing.T) {
+		pub := kinesis.New(client)
+		err := pub.ProduceBulk([]*pb.Event{testEvent}, "conn_group")
+		require.Error(t, err)
+
+	})
+	t.Run("should create the stream if it doesn't exist and autocreate is set to true", func(t *testing.T) {
+		pub := kinesis.New(client, kinesis.WithStreamAutocreate(true))
+		err := pub.ProduceBulk([]*pb.Event{testEvent}, "conn_group")
+		require.NoError(t, err)
+		_, err = client.DeleteStream(context.Background(), &kinesis_sdk.DeleteStreamInput{
+			StreamName: aws.String("click"),
+		})
+		require.NoError(t, err)
+	})
 }

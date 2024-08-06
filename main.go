c3933380f0b2db494e83c6616085ab1b549e4e6c
turtleDev <saravjeetamansingh@gmail.com>
2024-08-06 18:18:28 +0530

    cli: remove usused comments

--- main.go
+++ main.go
@@ -6,18 +6,6 @@ import (
 	"github.com/raystack/raccoon/cmd"
 )
 
-// func main() {
-// 	config.Load()
-// 	middleware.Load()
-// 	metrics.Setup()
-// 	logger.SetLevel(config.Log.Level)
-// 	err := app.Run()
-// 	metrics.Close()
-// 	if err != nil {
-// 		logger.Fatal("init failure", err)
-// 	}
-// }
-
 func main() {
 	if err := cmd.New().Execute(); err != nil {
 		os.Exit(1)

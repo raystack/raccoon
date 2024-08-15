7442e941d356356b6c470d9f432dbd682c749cc4
turtleDev <saravjeetamansingh@gmail.com>
2024-08-15 23:44:16 +0530

    config: fix tags on publisher config

--- config/publisher.go
+++ config/publisher.go
@@ -13,24 +13,24 @@ var Publisher publisher
 var dynamicKafkaClientConfigPrefix = "PUBLISHER_KAFKA_CLIENT_"
 
 type publisherPubSub struct {
-	ProjectId               string `mapstructure:"PUBLISHER_PUBSUB_PROJECT_ID" cmdx:"publisher.pubsub.project.id"`
-	TopicAutoCreate         bool   `mapstructure:"PUBLISHER_PUBSUB_TOPIC_AUTOCREATE" cmdx:"publisher.pubsub.topic.autocreate" default:"false"`
-	TopicRetentionPeriodMS  int64  `mapstructure:"PUBLISHER_PUBSUB_TOPIC_RETENTION_MS" cmdx:"publisher.pubsub.topic.retention.ms" default:"0"`
-	PublishTimeoutMS        int64  `mapstructure:"PUBLISHER_PUBSUB_PUBLISH_TIMEOUT_MS" cmdx:"publisher.pubsub.publish.timeout.ms" default:"60000"`
-	PublishDelayThresholdMS int64  `mapstructure:"PUBLISHER_PUBSUB_PUBLISH_DELAY_THRESHOLD_MS" cmdx:"publisher.pubsub.publish.delay.threshold.ms" default:"10"`
-	PublishCountThreshold   int    `mapstructure:"PUBLISHER_PUBSUB_PUBLISH_COUNT_THRESHOLD" cmdx:"publisher.pubsub.publish.count.threshold" default:"100"`
-	PublishByteThreshold    int    `mapstructure:"PUBLISHER_PUBSUB_PUBLISH_BYTE_THRESHOLD" cmdx:"publisher.pubsub.publish.byte.threshold" default:"1000000"`
-	CredentialsFile         string `mapstructure:"PUBLISHER_PUBSUB_CREDENTIALS" cmdx:"publisher.pubsub.credentials"`
+	ProjectId               string `mapstructure:"project_id" cmdx:"publisher.pubsub.project.id"`
+	TopicAutoCreate         bool   `mapstructure:"topic_autocreate" cmdx:"publisher.pubsub.topic.autocreate" default:"false"`
+	TopicRetentionPeriodMS  int64  `mapstructure:"topic_retention_ms" cmdx:"publisher.pubsub.topic.retention.ms" default:"0"`
+	PublishTimeoutMS        int64  `mapstructure:"publish_timeout_ms" cmdx:"publisher.pubsub.publish.timeout.ms" default:"60000"`
+	PublishDelayThresholdMS int64  `mapstructure:"publish_delay_threshold_ms" cmdx:"publisher.pubsub.publish.delay.threshold.ms" default:"10"`
+	PublishCountThreshold   int    `mapstructure:"publish_count_threshold" cmdx:"publisher.pubsub.publish.count.threshold" default:"100"`
+	PublishByteThreshold    int    `mapstructure:"publish_byte_threshold" cmdx:"publisher.pubsub.publish.byte.threshold" default:"1000000"`
+	CredentialsFile         string `mapstructure:"credentials" cmdx:"publisher.pubsub.credentials"`
 }
 
 type publisherKinesis struct {
-	Region                string `mapstructure:"PUBLISHER_KINESIS_AWS_REGION" cmdx:"publisher.kinesis.aws.region"`
-	CredentialsFile       string `mapstructure:"PUBLISHER_KINESIS_CREDENTIALS" cmdx:"publisher.kinesis.credentials"`
-	StreamAutoCreate      bool   `mapstructure:"PUBLISHER_KINESIS_STREAM_AUTOCREATE" cmdx:"publisher.kinesis.stream.autocreate" default:"false"`
-	StreamProbeIntervalMS int64  `mapstructure:"PUBLISHER_KINESIS_STREAM_PROBE_INTERVAL_MS" cmdx:"publisher.kinesis.stream.probe.interval.ms" default:"1000"`
-	StreamMode            string `mapstructure:"PUBLISHER_KINESIS_STREAM_MODE" cmdx:"publisher.kinesis.stream.mode" default:"ON_DEMAND"`
-	DefaultShards         uint32 `mapstructure:"PUBLISHER_KINESIS_STREAM_SHARDS" cmdx:"publisher.kinesis.stream.shards" default:"4"`
-	PublishTimeoutMS      int64  `mapstructure:"PUBLISHER_KINESIS_PUBLISH_TIMEOUT_MS" cmdx:"publisher.kinesis.publish.timeout.ms" default:"60000"`
+	Region                string `mapstructure:"aws_region" cmdx:"publisher.kinesis.aws.region"`
+	CredentialsFile       string `mapstructure:"credentials" cmdx:"publisher.kinesis.credentials"`
+	StreamAutoCreate      bool   `mapstructure:"stream_autocreate" cmdx:"publisher.kinesis.stream.autocreate" default:"false"`
+	StreamProbeIntervalMS int64  `mapstructure:"stream_probe_interval_ms" cmdx:"publisher.kinesis.stream.probe.interval.ms" default:"1000"`
+	StreamMode            string `mapstructure:"stream_mode" cmdx:"publisher.kinesis.stream.mode" default:"ON_DEMAND"`
+	DefaultShards         uint32 `mapstructure:"stream_shards" cmdx:"publisher.kinesis.stream.shards" default:"4"`
+	PublishTimeoutMS      int64  `mapstructure:"publish_timeout_ms" cmdx:"publisher.kinesis.publish.timeout.ms" default:"60000"`
 }
 
 type publisherKafka struct {

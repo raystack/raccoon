3a499e704117b04eaca95bbe6a037bdb8befb4e5
turtleDev <saravjeetamansingh@gmail.com>
2024-08-19 14:00:07 +0530

    cmd: server: simplify flag parsing

--- cmd/server.go
+++ cmd/server.go
@@ -41,36 +41,26 @@ func serverCommand() *cobra.Command {
 
 func bindFlag(flag *pflag.FlagSet, ref any, meta reflect.StructField) {
 
-	el := reflect.ValueOf(ref).Elem()
-	kind := el.Kind()
-	typ := el.Type()
 	flagName := meta.Tag.Get("cmdx")
 	desc := meta.Tag.Get("desc")
 
-	switch {
-	case typ.Name() == "AckType":
-		v := ref.(*config.AckType)
+	switch v := ref.(type) {
+	case *config.AckType:
 		flag.Var(ackTypeFlag{v}, flagName, desc)
-	case kind == reflect.String:
-		v := ref.(*string)
+	case *string:
 		flag.StringVar(v, flagName, *v, desc)
-	case kind == reflect.Int:
-		v := ref.(*int)
+	case *int:
 		flag.IntVar(v, flagName, *v, desc)
-	case kind == reflect.Int64:
-		v := ref.(*int64)
+	case *int64:
 		flag.Int64Var(v, flagName, *v, desc)
-	case kind == reflect.Uint32:
-		v := ref.(*uint32)
+	case *uint32:
 		flag.Uint32Var(v, flagName, *v, desc)
-	case kind == reflect.Bool:
-		v := ref.(*bool)
+	case *bool:
 		flag.BoolVar(v, flagName, *v, desc)
-	case kind == reflect.Slice && typ.Elem().String() == "string":
-		v := ref.(*[]string)
+	case *[]string:
 		flag.StringSliceVar(v, flagName, *v, desc)
 	default:
-		msg := fmt.Sprintf("unsupport flag. kind = %s, type = %s", kind, typ)
+		msg := fmt.Sprintf("unsupport flag of type %T", ref)
 		panic(msg)
 	}
 }

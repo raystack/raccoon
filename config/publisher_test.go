ad765fdf7f22ac2d5fe69993564c1b8ca35ee7ae
turtleDev <saravjeetamansingh@gmail.com>
2024-09-04 23:27:58 +0530

    config: kafka: add more tests for config map serializer

--- config/publisher_test.go
+++ config/publisher_test.go
@@ -8,16 +8,22 @@ import (
 )
 
 func TestKafkaPublisher(t *testing.T) {
-	p := publisherKafka{
-		ClientConfig: kafkaClientConfig{
-			BootstrapServers: "localhost:8082",
-			Acks:             "1",
-		},
-	}
+	t.Run("should convert client config to equivalent config map", func(t *testing.T) {
+		p := publisherKafka{
+			ClientConfig: kafkaClientConfig{
+				BootstrapServers: "localhost:8082",
+				Acks:             "1",
+			},
+		}
 
-	cm := p.ToKafkaConfigMap()
-	expected := &kafka.ConfigMap{}
-	expected.SetKey("bootstrap.servers", "localhost:8082")
-	expected.SetKey("acks", "1")
-	assert.Equal(t, cm, expected)
+		cm := p.ToKafkaConfigMap()
+		expected := &kafka.ConfigMap{}
+		expected.SetKey("bootstrap.servers", "localhost:8082")
+		expected.SetKey("acks", "1")
+		assert.Equal(t, cm, expected)
+	})
+
+	t.Run("should only add configs to config map that have non-zero values", func(t *testing.T) {
+		assert.Equal(t, &kafka.ConfigMap{}, publisherKafka{}.ToKafkaConfigMap())
+	})
 }

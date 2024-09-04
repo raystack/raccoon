5cc9f1aa8ceb09c5e57aaab5b800975caba3cf71
turtleDev <saravjeetamansingh@gmail.com>
2024-09-04 17:13:23 +0530

    worker: use idiomatic null device for tests

--- worker/init_test.go
+++ worker/init_test.go
@@ -1,18 +1,14 @@
 package worker
 
 import (
+	"io"
 	"os"
 	"testing"
 
 	"github.com/raystack/raccoon/logger"
 )
 
-type void struct{}
-
-func (v void) Write(_ []byte) (int, error) {
-	return 0, nil
-}
 func TestMain(t *testing.M) {
-	logger.SetOutput(void{})
+	logger.SetOutput(io.Discard)
 	os.Exit(t.Run())
 }

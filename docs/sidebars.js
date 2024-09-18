97af73d11c079940989856f2b5b8b278d632e0a6
turtleDev <saravjeetamansingh@gmail.com>
2024-09-18 21:24:32 +0530

    docs: add skeleton for clients

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -16,6 +16,17 @@ module.exports = {
     },
     {
       type: "category",
+      label: "Clients",
+      items: [
+        "clients/overview",
+        "clients/golang",
+        "clients/java",
+        "clients/javascript",
+        "clients/python",
+      ],
+    },
+    {
+      type: "category",
       label: "Concepts",
       items: [
         "concepts/architecture",
